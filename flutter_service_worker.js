7eebe4fe5ea5027ed7f7fd370400070280d4ec75
Mehdi667 <mehdi667@hotmail.com>
2020-06-09 19:21:40 +0430

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "66077b7d3c38af1b168e204e29e7443a",
 "index.html": "06e03b8ed258a2cf8f143293ac77e831",
 "/": "06e03b8ed258a2cf8f143293ac77e831",
-"main.dart.js": "f5da689b91bdc74e692530312c2b645d",
+"main.dart.js": "f0d94f877a9a509287a3fd6328811a46",
 "manifest.json": "e5005b7c8eafa646a807278f51f6679d"
 };
 
